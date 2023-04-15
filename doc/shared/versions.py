c2c96f4fdd27a776dbb20b005d080f52640d1891
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2023-04-15 16:19:11 -0500

    first commit

---
+++ doc/shared/versions.py
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------
+# SUNDIALS Copyright Start
+# Copyright (c) 2002-2023, Lawrence Livermore National Security
+# and Southern Methodist University.
+# All rights reserved.
+#
+# See the top-level LICENSE and NOTICE files for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SUNDIALS Copyright End
+# ----------------------------------------------------------------
+sundials_version = 'v6.5.1'
+arkode_version = 'v5.5.1'
+cvode_version = 'v6.5.1'
+cvodes_version = 'v6.5.1'
+ida_version = 'v6.5.1'
+idas_version = 'v5.5.1'
+kinsol_version = 'v6.5.1'
+year = '2023'
